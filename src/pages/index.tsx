01513193181b441b46b474e81ca79e67796889c9
Jubaer Jami <jamijubaer@gmail.com>
2023-08-02 23:51:04 +0600

    added glassy boxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,113 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const HindSiliguri = localFont({
+  src: [
+    {
+      path: "Fonts/Hind_Siliguri/HindSiliguri-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "Fonts/Hind_Siliguri/HindSiliguri-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "Fonts/Hind_Siliguri/HindSiliguri-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "Fonts/Hind_Siliguri/HindSiliguri-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "Fonts/Hind_Siliguri/HindSiliguri-Medium.ttf",
+      weight: "500",
+    },
+  ],
+  variable: "--font-HindSiliguri",
+});
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`  ${HindSiliguri.variable} font-hind`}>
+      <div className="bg-[#0B060D] min-h-[100vh] p-10 ">
+        <div className="flex gap-20 flex-wrap justify-center">
+         
+
+          <div
+            className="rounded-xl max-w-[400px] text-center"
+            style={{
+              background:
+                "linear-gradient(rgba(202, 101, 253, 0.44) 0%, rgba(177, 83, 224, 0) 100%)",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div
+              className=" rounded-xl px-6 py-8 pb-20 mx-[1px] bg-[#0B060D]/80 relative top-[1px]"
+              style={{}}
+            >
+              <div className="">
+                <p className="text-heading text-center text-xl">
+                  ক্যারিয়ার গঠন
+                </p>
+                <p className="text-paragraph mt-8">
+                  CP তোমাকে শক্তপোক্ত একটা রেজুমে গড়তে সাহায্য করে যা ভবিষ্যতের
+                  ড্রিম কোম্পানি ডাক পেতে এগিয়ে রাখবে লাখো ক্যান্ডিডেট থেকে যারা
+                  CP করে নাই
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            className="rounded-xl max-w-[400px] text-center"
+            style={{
+              background:
+                "linear-gradient(rgba(202, 101, 253, 0.44) 0%, rgba(177, 83, 224, 0) 100%)",
+            }}
+          >
+            <div
+              className=" rounded-xl px-6 py-8 pb-20 mx-[1px] bg-[#0B060D]/80 relative top-[1px]"
+              style={{}}
+            >
+              <div className="">
+                <p className="text-heading text-center text-xl">
+                  ক্যারিয়ার গঠন
+                </p>
+                <p className="text-paragraph mt-8">
+                  CP তোমাকে শক্তপোক্ত একটা রেজুমে গড়তে সাহায্য করে যা ভবিষ্যতের
+                  ড্রিম কোম্পানি ডাক পেতে এগিয়ে রাখবে লাখো ক্যান্ডিডেট থেকে যারা
+                  CP করে নাই
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            className="rounded-xl max-w-[400px] text-center"
+            style={{
+              background:
+                "linear-gradient(rgba(202, 101, 253, 0.44) 0%, rgba(177, 83, 224, 0) 100%)",
+            }}
+          >
+            <div
+              className=" rounded-xl px-6 py-8 pb-20 mx-[1px] bg-[#0B060D]/80 relative top-[1px]"
+              style={{}}
+            >
+              <div className="">
+                <p className="text-heading text-center text-xl">
+                  ক্যারিয়ার গঠন
+                </p>
+                <p className="text-paragraph mt-8">
+                  CP তোমাকে শক্তপোক্ত একটা রেজুমে গড়তে সাহায্য করে যা ভবিষ্যতের
+                  ড্রিম কোম্পানি ডাক পেতে এগিয়ে রাখবে লাখো ক্যান্ডিডেট থেকে যারা
+                  CP করে নাই
+                </p>
+              </div>
+            </div>
+          </div>
+       
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        hind: ["var(--font-HindSiliguri)", ...fontFamily.sans],
+      },
+      colors:{
+        teal:"#107B61",
+        yellow:"#DE9931",
+        green:"#31BFDE",
+        purple:"#B153E0",
+        paragraph:"#BCBCBC",
+        heading:"#E2E8F0"
+      }
     },
   },
   plugins: [],
-}
+};
