74818dda45910f9169eed7425c53cb4d6ae8d734
Jubaer Jami <jamijubaer@gmail.com>
2023-09-24 02:15:45 +0600

    updatess

--- src/components/FloatingCompiler.tsx
+++ src/components/FloatingCompiler.tsx
@@ -102,7 +102,7 @@ int main(){
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className=" w-[90vw]  transform overflow-hidden rounded-2xl bg-[#0B060D] bg-opacity-30  backdrop-blur-lg border border-gray-200/20 p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className=" w-[90vw]  text-darkHeading transform overflow-hidden rounded-2xl bg-[#0B060D]/60 dark:bg-[#0B060D]/30 bg-opacity-30  backdrop-blur-lg border border-gray-200/20 p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="div"
                     className="text-lg font-medium leading-6 "
