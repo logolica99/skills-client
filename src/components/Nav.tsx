cfcf5f3085790b5f04265cffecca84ec8724c54b
logolica99 <jamijubaer@gmail.com>
2024-03-27 23:03:59 +0600

    added dark loogo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -113,7 +113,7 @@ export default function Nav({}: Props) {
         <div className="w-[90%]  mx-auto py-4">
           <div className="flex justify-between items-center ">
             <div className="flex gap-10 items-center">
-              <Link href="/" className="">
+              <Link href="/" className="hidden dark:block">
                 <img src="/logo.png" alt="" className="w-8 md:w-14 mr-8 " />
               </Link>
               <Link href="/" className="dark:hidden">
