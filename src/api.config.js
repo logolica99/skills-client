69083c8aed72f2d426f2f5449fcea81689ee8421
logolica99 <jamijubaer@gmail.com>
2024-09-08 19:13:23 +0600

    updated api url

--- src/api.config.js
+++ src/api.config.js
@@ -1,7 +1,7 @@
-export const BACKEND_URL = "http://192.168.68.73:5000";
+// export const BACKEND_URL = "http://192.168.68.73:5000";
 // export const BACKEND_URL = "https://binary-school-dev.onrender.com";
-// export const BACKEND_URL =
-//   "https://2m06xslkj8.execute-api.ap-southeast-2.amazonaws.com/prod";
+export const BACKEND_URL =
+  "https://2m06xslkj8.execute-api.ap-southeast-2.amazonaws.com/prod";
 // export const BACKEND_URL =
 //   "http://localhost:4000";
 export const COURSE_ID = "1";
