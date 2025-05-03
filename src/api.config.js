9b9e02cc874f05d78f83ce75a729b6fb799f6563
emran-mostofa <jamijubaer@gmail.com>
2025-05-04 04:27:48 +0600

    changed the config to backend repo

--- src/api.config.js
+++ src/api.config.js
@@ -1,9 +1,9 @@
 // export const BACKEND_URL = "http://192.168.68.59:5000";
 // export const BACKEND_URL = "http://192.168.68.73:5000";
 // export const BACKEND_URL = "https://binary-school-dev.onrender.com";
-// export const BACKEND_URL =
-//   "https://2m06xslkj8.execute-api.ap-southeast-2.amazonaws.com/prod";
 export const BACKEND_URL =
-  "http://localhost:4000";
+  "https://2m06xslkj8.execute-api.ap-southeast-2.amazonaws.com/prod";
+// export const BACKEND_URL =
+//   "http://localhost:4000";
 export const COURSE_ID = "1";
 export const COURSE_ID_2 = "15";
